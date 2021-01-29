813c6804a0a9843df6577f294fdcc21db6a59bdb
Goncagul Ay <ggloria.ay@gmail.com>
2021-01-29 17:23:04 -0500

    feature: signup/partial signin

---
+++ src/components/LandingPage.js
@@ -0,0 +1,19 @@
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <Route path="/signup" component={SignUp} />
+          <Route path="/signin" component={SignIn} />
+        </Switch>
+      </AuthProvider>
+    </Router>
+  );
+}
