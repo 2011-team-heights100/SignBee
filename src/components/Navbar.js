f6ce664940f7dfddb3b835361c46c76ea43b8a47
Julia Kravets <juliaakravets@gmail.com>
2021-02-02 18:03:43 -0500

    played with styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,26 @@
 import React from "react";
-import {AppBar, Toolbar, IconButton} from "@material-ui/core";
+import { AppBar, Grid, Toolbar, IconButton } from "@material-ui/core";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 
-import ProfileSummary from "./ProfileSummary"
+import ProfileSummary from "./ProfileSummary";
 
 export default function Navbar() {
+   
+const handleClick = (e) => {
+   this.preventDefault()
+
+}
+
 	return (
 		<>
 			<AppBar position="static" color="transparent" elevation={0}>
 				<Toolbar>
-					<IconButton >
-						<AccountCircle />
-					</IconButton>
+					<Grid container justify="space-between">
+                  <div></div>
+						<IconButton color="secondary" onClick={handleClick}>
+							<AccountCircle />
+						</IconButton>
+					</Grid>
 				</Toolbar>
 			</AppBar>
 			<ProfileSummary />
