42cf3dd74b9504b58f86beb027ca2e7673a58c4b
Goncagul Ay <ggloria.ay@gmail.com>
2021-02-04 11:44:14 -0500

    feature: added heading and prompt box

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,30 +5,30 @@ import { useAuth } from "../contexts/AuthContext";
 import ProfileSummary from "./ProfileSummary";
 
 export default function Navbar() {
-   const { dbUser, currentUser } = useAuth();
-   const [modalShow, setModalShow ] = useState(false)
+  const { dbUser, currentUser } = useAuth();
+  const [modalShow, setModalShow] = useState(false);
 
-	const handleClick = (e) => {
-      e.preventDefault();
-      setModalShow(!modalShow)
-	};
-	
-	return (
-		<>
-			<AppBar position="static" color="transparent" elevation={0}>
-				<Toolbar>
-					<Grid container justify="space-between">
-						<div></div>
-						<Link to="/app">APP</Link>
-						{dbUser && (
-							<IconButton color="secondary" onClick={handleClick}>
-								<AccountCircle />
-							</IconButton>
-						)}
-					</Grid>
-				</Toolbar>
-			</AppBar>
-			<ProfileSummary show={modalShow} />
-		</>
-	);
+  const handleClick = (e) => {
+    e.preventDefault();
+    setModalShow(!modalShow);
+  };
+
+  return (
+    <>
+      <AppBar position="static" color="transparent" elevation={0}>
+        <Toolbar>
+          <Grid container justify="space-between">
+            <div></div>
+            <Link to="/app">START</Link>
+            {dbUser && (
+              <IconButton color="secondary" onClick={handleClick}>
+                <AccountCircle />
+              </IconButton>
+            )}
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <ProfileSummary show={modalShow} />
+    </>
+  );
 }
